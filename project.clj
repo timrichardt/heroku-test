92e3b381d565922f4dfb4cf8f5bf105a7b739375
Tim Richardt <t.richardt@confidential.portal.film>
2022-06-27 13:42:55 +0200

    chore: cleanup

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojure-getting-started "1.0.0-SNAPSHOT"
-  :description "Demo Clojure web app"
-  :url "http://clojure-getting-started.herokuapp.com"
+  :description "heroku test"
+  :url "https://morning-inlet-68068.herokuapp.com/"
   :license {:name "Eclipse Public License v1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
@@ -10,5 +10,5 @@
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.3.1"]]
   :hooks [environ.leiningen.hooks]
-  :uberjar-name "clojure-getting-started-standalone.jar"
+  :uberjar-name "heroku-test.jar"
   :profiles {:production {:env {:production true}}})
