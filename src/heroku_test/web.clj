300c684c7974f007e5b5df54126d88c7562b5ed2
Tim Richardt <t.richardt@confidential.portal.film>
2022-06-27 17:00:41 +0200

    feat: faster digit sequence

--- src/heroku_test/web.clj
+++ src/heroku_test/web.clj
@@ -5,6 +5,50 @@
             [environ.core :refer [env]]))
 
 
+;; ----------------------------------------
+;; Hashing function
+
+(defn- int->digit-sequence
+  "Takes integer `number` and returns sequence of its digits in base 10."
+  ([number]
+   {:pre (int? number)}
+   (int->digit-sequence number []))
+  ([number digits]
+   (if (zero? number)
+     (reverse digits)
+     (let [remainder (mod number 10)]
+       (recur (/ (- number remainder) 10) (conj digits remainder))))))
+
+
+(defn- int->digit-sequence-2
+  "Takes integer `number` and returns sequence of its digits in base 10.
+
+  Another way to `int->digit-sequence`, seems to be faster.
+
+  (criterium.core/bench (int->digit-sequence-2 123456789012345))
+               Execution time mean : 57.865278 ns
+
+  (criterium.core/bench (int->digit-sequence 123456789012345))
+               Execution time mean : 2.482377 µs"
+  [number]
+  (->> (str number)
+       (map #(Integer/parseInt (str %)))))
+
+
+(defn hash
+  "Compute some hash of integer sequence `values`.
+
+  The hash is the digit sum of the sum of `values`."
+  [values]
+  (->> values
+       (reduce +)
+       int->digit-sequence-2
+       (reduce +)))
+
+
+;; ----------------------------------------
+;; Request handlers
+
 (defn NOT_FOUND
   "Status 404 Not Found response."
   [_]
@@ -29,29 +73,6 @@
    :body    body})
 
 
-(defn- int->digit-sequence
-  "Takes integer `number` and returns sequence of its digits in base 10."
-  ([number]
-   {:pre (int? number)}
-   (int->digit-sequence number []))
-  ([number digits]
-   (if (zero? number)
-     (reverse digits)
-     (let [remainder (mod number 10)]
-       (recur (/ (- number remainder) 10) (conj digits remainder))))))
-
-
-(defn hash
-  "Compute some hash of integer sequence `values`.
-
-  The hash is the digit sum of the sum of `values`."
-  [values]
-  (->> values
-       (reduce +)
-       int->digit-sequence
-       (reduce +)))
-
-
 (defn hash-handler
   "Handles hash input validation and computation."
   [request]  
@@ -67,12 +88,18 @@
           (OK {:result (hash values)}))))
 
 
+;; ----------------------------------------
+;; Routing
+
 (defroutes service
   (POST "/" [] (->> hash-handler
                     wrap-json-body
                     wrap-json-response))
   (ANY "*" [] NOT_FOUND))
 
+
+;; ----------------------------------------
+;; Initialization
 
 (defn -main
   ""
